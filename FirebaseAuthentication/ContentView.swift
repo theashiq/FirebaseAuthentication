ab03b8cccc9415e4aa1aded420511dfe763da161
Mac Mini <>
2023-12-13 22:44:58 +0600

    Project Created

---
+++ FirebaseAuthentication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseAuthentication
+//
+//  Created by mac 2019 on 12/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseAuthentication/FirebaseAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthenticationApp.swift
+//  FirebaseAuthentication
+//
+//  Created by mac 2019 on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
